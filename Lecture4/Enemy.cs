9eb462998fa140a47a1fbfbe015d4fcb2601e554
Kwame Bryan <kwame@3einteractive.com>
2015-11-06 12:12:00 -0500

    Added Textbased Project

---
+++ Lecture4/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture4
+{
+    class Enemy
+    {
+        // type // name // value
+       string name;
+       public int weaponCount;
+       public bool isAlive;
+       public int currentState;
+       // getter and setter
+       public string Name
+       {
+           get
+           {
+               return name;
+           }
+           set
+           {
+               name = value;
+           }
+       }
+
+        public Enemy()
+        {
+            name = "Enemy";
+            weaponCount = 5;
+            isAlive = true;
+            currentState = 2;
+        }
+        public Enemy(string enemyName, int weapon, bool alive, int state)
+        {
+            name = enemyName;
+            weaponCount = weapon;
+            isAlive = alive;
+            currentState = state;
+        }
+        public void enemyAttack()
+        {
+            Console.WriteLine("Attacking Player");
+        }
+    }
+}
---
+++ Lecture4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy firstEnemy = new Enemy();
+            Enemy secondEnemy = new Enemy("Enemy1", 3, true, 1);
+
+            string myPlayerName;
+            string myDecision;
+            firstEnemy.enemyAttack();
+            Console.WriteLine("Enter Player Name");
+            myPlayerName = Console.ReadLine();
+            Console.WriteLine("Welcome {0}, to our game", myPlayerName);
+            Console.Read();
+            Console.WriteLine("Enter cavern y / n");
+            myDecision = Console.ReadLine();
+           
+            Console.Read();
+        }
+    }
+}
